36299c26f17ebaae47615581dda9fe0d94eb9bf3
김경석 <drather_dev@naver.com>
2022-03-17 17:54:23 +0900

    feat: repository 주석 추가

--- app/application/interfaces/repository.py
+++ app/application/interfaces/repository.py
@@ -3,15 +3,32 @@ import abc
 from app.domain.entity import User, Domain
 
 
+"""
+고수준 구현체 Repository
+"""
 class AbstractRepository(abc.ABC):
     @abc.abstractmethod
     def create(self, model: Domain):
+        """
+        생성 메서드
+        :param model:
+        :return:
+        """
         ...
 
     @abc.abstractmethod
     def find_one(self, model:Domain):
+        """
+        검색 메서드
+        :param model:
+        :return:
+        """
         ...
 
     @abc.abstractmethod
     def find_all(self):
+        """
+        전체 조회 메서드
+        :return:
+        """
         ...
