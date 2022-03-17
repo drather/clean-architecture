331e5dd3d51169715cd627abeb9986622d2889d9
김경석 <drather_dev@naver.com>
2022-03-17 17:54:02 +0900

    feat: product.py 주석 추가

--- app/application/services/product.py
+++ app/application/services/product.py
@@ -4,14 +4,27 @@ from app.domain.entity import Product
 
 class ProductService:
     def __init__(self, repository: AbstractRepository):
+        """
+        repository 를 외부에서 주입 받는 생성 메서드
+        :param repository:
+        """
         self.repository = repository
 
     def get_product(self, product_id: int):
+        """
+        상품 검색 메서드
+        :param product_id:
+        :return:
+        """
         _product = Product(id=product_id)
         product = self.repository.find_one(model=_product)
 
         return product
 
     def get_product_all(self):
+        """
+        전체 조회 메서드
+        :return:
+        """
         all_products = self.repository.find_all()
         return all_products
